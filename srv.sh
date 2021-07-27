f91729e0b27c77307471d0865895b00901715d25
Devill Don <53294521+devillD@users.noreply.github.com>
2021-07-27 12:05:46 +0600

    Rclone server

--- srv.sh
+++ srv.sh
@@ -1,8 +1,8 @@
 wget $SAFILE -O sa.json && \
 wget $CONFIG -O .config/rclone/rclone.conf && \
-rclone serve http $NAME: --addr :$PORT --buffer-size 128M \
+rclone serve http $NAME: --addr :$PORT --user $USER \
+                                       --pass $PASS \
                                        --vfs-cache-mode writes \
-                                       --vfs-read-chunk-size 256M \
-                                       --user $USER --pass $PASS \
+                                       --vfs-read-chunk-size 128M \
                                        --vfs-read-chunk-size-limit 1G
 # For more, visit: https://rclone.org/commands/rclone_serve_http/ and https://rclone.org/flags/
